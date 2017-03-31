8e5deb995169d9b22152e8d4caa1ea53cb94fa56
Perin <perin@rech.com.br>
2017-03-31 12:38:26 -0300

    Criado projeto

---
+++ src/br/com/perin/core/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Main
+ * CopyRight Rech Informática Ltda. Todos os direitos reservados.
+ */
+package br.com.perin.core;
+
+/**
+ *
+ * @author Perin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MyServlet().init();
+    }
+
+}
---
+++ src/br/com/perin/core/MyServlet.java
@@ -0,0 +1,16 @@
+/*
+ * MyServlet
+ * CopyRight Rech Informática Ltda. Todos os direitos reservados.
+ */
+package br.com.perin.core;
+
+/**
+ * Descrição da classe.
+ */
+public class MyServlet {
+
+    public void init() {
+        System.out.println("MyServlet.");
+    }
+
+}
